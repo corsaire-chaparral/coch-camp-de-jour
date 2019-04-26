db2f6a2b6436c9e6e50227578c8a8f0eb7865b4c
Louis-Olivier Brassard <louis@loupbrun.ca>
2019-04-26 15:10:10 -0400

    Post-order message - fix bad unescaped html strings
    This should be submitted to main repo

--- resources/views/Public/ViewEvent/Partials/EventViewOrderSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventViewOrderSection.blade.php
@@ -69,7 +69,7 @@
 
                 @if($event->post_order_display_message)
                 <div class="alert alert-dismissable alert-info">
-                    {{ nl2br(e($event->post_order_display_message)) }}
+                    {!! nl2br(e($event->pre_order_display_message)) !!}
                 </div>
                 @endif
 
