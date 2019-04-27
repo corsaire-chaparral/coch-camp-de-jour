5ebe5c66b41f1956aae5d28077f57e9cdd36836e
Louis-Olivier Brassard <louis@loupbrun.ca>
2019-04-27 15:18:32 -0400

    Favicons : add and replace all icons for Corsaire-Chaparral (#4)
    Also app colors metadata, android icon, apple-touch-icons

--- resources/views/Shared/Partials/GlobalMeta.blade.php
+++ resources/views/Shared/Partials/GlobalMeta.blade.php
@@ -1,28 +1,24 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{ asset('assets/images/touch/favicon.ico')  }}">
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset("assets/images/touch/apple-touch-icon-57x57.png") }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset("assets/images/touch/apple-touch-icon-114x114.png") }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset("assets/images/touch/apple-touch-icon-72x72.png") }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset("assets/images/touch/apple-touch-icon-144x144.png") }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset("assets/images/touch/apple-touch-icon-60x60.png") }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset("assets/images/touch/apple-touch-icon-120x120.png") }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset("assets/images/touch/apple-touch-icon-76x76.png") }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset("assets/images/touch/apple-touch-icon-152x152.png") }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("assets/images/touch/apple-touch-icon-180x180.png") }}">
-    <meta name="apple-mobile-web-app-title" content="Attendize">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset("assets/images/touch/favicon-192x192.png") }}">
-    <link rel="icon" type="image/png" sizes="160x160" href="{{ asset("assets/images/touch/favicon-160x160.png") }}">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0"><link rel="apple-touch-icon" sizes="57x57" href="{{ asset("assets/images/touch/apple-icon-57x57.png") }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset("assets/images/touch/apple-icon-60x60.png") }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset("assets/images/touch/apple-icon-72x72.png") }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset("assets/images/touch/apple-icon-76x76.png") }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset("assets/images/touch/apple-icon-114x114.png") }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset("assets/images/touch/apple-icon-120x120.png") }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset("assets/images/touch/apple-icon-144x144.png") }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset("assets/images/touch/apple-icon-152x152.png") }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("assets/images/touch/apple-icon-180x180.png") }}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset("assets/images/touch/android-icon-192x192.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("assets/images/touch/favicon-32x32.png") }}">
     <link rel="icon" type="image/png" sizes="96x96" href="{{ asset("assets/images/touch/favicon-96x96.png") }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("assets/images/touch/favicon-16x16.png") }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("assets/images/touch/favicon-32x32.png") }}">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="{{ url("assets/images/touch/mstile-144x144.png") }}">
-    <meta name="msapplication-config" content="{{ url("assets/images/touch/browserconfig.xml") }}">
-    <meta name="application-name" content="Attendize">
+    <link rel="manifest" href="{{ asset("assets/images/touch/manifest.json") }}">
+    <meta name="msapplication-TileColor" content="#ff6a13">
+    <meta name="msapplication-TileImage" content="{{ asset("assets/images/touch/ms-icon-144x144.png") }}">
+    <meta name="application-name" content="Camp de jour Corsaire-Chaparral">
     <meta name="_token" content="{{ csrf_token() }}" />
     {{--Mobile browser theme colors--}}
-    <meta name="theme-color" content="#2E3254">
-    <meta name="msapplication-navbutton-color" content="#2E3254">
-    <meta name="apple-mobile-web-app-status-bar-style" content="#2E3254">
+    <meta name="theme-color" content="#ff6a13">
+    <meta name="msapplication-navbutton-color" content="#ff6a13">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ff6a13">
