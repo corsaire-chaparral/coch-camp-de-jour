a0dc3f50ac83e50fb70cb1db60713c84d09569fc
Louis-Olivier Brassard <louis@loupbrun.ca>
2019-04-26 20:51:01 -0400

    Correct sales volume on dashboard
    Sales volume already included fees; do not add fees again on this total!

--- resources/views/ManageOrganiser/Dashboard.blade.php
+++ resources/views/ManageOrganiser/Dashboard.blade.php
@@ -75,7 +75,7 @@
         <div class="col-sm-4">
             <div class="stat-box">
                 <h3>
-                    {{ money($organiser->events->sum('sales_volume') + $organiser->events->sum('organiser_fees_volume'), $organiser->account->currency) }}
+                    {{ money($organiser->events->sum('sales_volume'), $organiser->account->currency) }}
                 </h3>
             <span>
                 @lang("Organiser.sales_volume")
