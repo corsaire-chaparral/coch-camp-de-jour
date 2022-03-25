7a6b422aa859d21fb085230f0e3e067f25f23d4b
Louis-Olivier Brassard <louis@loupbrun.ca>
2022-03-25 09:03:33 -0400

    ne pas afficher share sur page confirmation

--- resources/views/Public/ViewEvent/Embedded/EventPageViewOrder.blade.php
+++ resources/views/Public/ViewEvent/Embedded/EventPageViewOrder.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     @include('Public.ViewEvent.Partials.EventShareSection')
-    @include('Public.ViewEvent.Partials.EventViewOrderSection')
+    {{--@include('Public.ViewEvent.Partials.EventViewOrderSection')--}}
     @include('Public.ViewEvent.Embedded.Partials.PoweredByEmbedded')
 @stop
